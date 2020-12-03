009f612491f83e06ef9928ed2d4a45b3239c4cfe
sourabh <sourabhkattimani01@gmail.com>
2020-12-03 14:20:38 +0530

    cart

--- server.js
+++ server.js
@@ -5,13 +5,23 @@ const path=require('path')
 const expresslayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 app.use(express.static('public'))
+
+app.use(expresslayout)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
 app.get('/',(req,res)=>{
     res.render("home")
 }
 )
-app.use(expresslayout)
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs')
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+app.get('/login',(req,res)=>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res)=>{
+    res.render('auth/register')
+})
 app.listen (PORT,()=>{
   console.log(`listing on port dfg  ${PORT}`)
 })
